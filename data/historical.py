a80005b821a39ec284435a13224dd61fd1447c5d
Aditya Pathak <aditya.pathak@me.com>
2023-01-17 15:32:55 +0300

    bug fix

--- data/historical.py
+++ data/historical.py
@@ -2,18 +2,28 @@ import json
 import os
 from boto3.dynamodb.conditions import Key, Attr
 import boto3
+import decimal
+import json
 
 dynamodb = boto3.resource('dynamodb')
 
 
-def query_by_time(event, context):
+# This is a workaround for: http://bugs.python.org/issue16535
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            return int(obj)
+        return super(DecimalEncoder, self).default(obj)
 
+
+def query_by_time(event, context):
     table_name = os.environ['TELEMATICS_TABLE']
     table = dynamodb.Table(table_name)
 
     result = table.query(
-        KeyConditionExpression=Key('imei').eq(event['pathParameters']['imei']) & Key('datetime').between(event['pathParameters']['start'],
-                                                   event['pathParameters']['end']),
+        KeyConditionExpression=Key('imei').eq(event['pathParameters']['imei']) & Key('datetime').between(
+            event['pathParameters']['start'],
+            event['pathParameters']['end']),
     )
     if result['Items']:
 
@@ -23,12 +33,12 @@ def query_by_time(event, context):
             "headers": {
                 "Access-Control-Allow-Origin": "*",
             },
-            "body": json.dumps(result['Items'])
+            "body": json.dumps(result['Items'], cls=DecimalEncoder)
         }
     else:
         response = {
             "statusCode": 200,
-            "body": json.dumps('No result'),
+            "body": json.dumps('No result', cls=DecimalEncoder),
             "headers": {
                 "Access-Control-Allow-Origin": "*",
             }
